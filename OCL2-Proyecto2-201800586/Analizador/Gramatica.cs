a9f6adef4c4d0bf08db00f503f2ce44f741e4387
SevenPok <57465474+SevenPok@users.noreply.github.com>
2021-04-02 19:00:54 -0600

    Initial commit

---
+++ OCL2-Proyecto2-201800586/Analizador/Gramatica.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Irony.Ast;
+using Irony.Parsing;
+
+namespace OCL2_Proyecto2_201800586.Analizador
+{
+    class Gramatica: Grammar
+    {
+        public Gramatica(): base (caseSensitive: false) {
+            #region ER
+            StringLiteral CADENA = new StringLiteral("Cadena", "'");
+            var ENTERO = new NumberLiteral("Entero");
+            var DECIMAL = new RegexBasedTerminal("Decimal", "[0-9]+'.'[0-9]+");
+            IdentifierTerminal IDENTIFICADOR = new IdentifierTerminal("ID");
+            CommentTerminal comentarioLinea = new CommentTerminal("comentarioLinea", "//", "\n", "\r\n");
+            CommentTerminal comentarioBloque = new CommentTerminal("comentarioBloque", "(*", "*)");
+            CommentTerminal comentarioBloque2 = new CommentTerminal("comentarioBloque", "{", "}");
+            #endregion
+
+            #region Terminales
+
+            #endregion
+
+            #region No Terminales
+
+            #endregion
+
+            #region Gramatica
+
+            #endregion
+
+        }
+    }
+}
